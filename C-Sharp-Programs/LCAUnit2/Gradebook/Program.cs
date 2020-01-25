8a256c80658cbe0e6a49181c20c0cd1ebc81df73
gsanders32 <gray.sanders@gmail.com>
2020-01-24 22:23:26 -0600

    Towers Of Hanoi

--- C-Sharp-Programs/LCAUnit2/Gradebook/Program.cs
+++ C-Sharp-Programs/LCAUnit2/Gradebook/Program.cs
@@ -10,8 +10,7 @@ namespace Gradebook
         static Dictionary<string, string> students = new Dictionary<string, string>(); // create student dictionary
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to your Gradebook!");
-            
+            Console.WriteLine("Welcome to your Gradebook!");           
             AddStudent(); //call method 
         }
 
@@ -20,12 +19,28 @@ namespace Gradebook
             do
             {
                 Console.WriteLine("Enter the students name or type quit to stop and see results");
-                string userStudentName = Console.ReadLine();
+                string userStudentName = Console.ReadLine().Trim();
                 if (userStudentName.ToLower() != "quit" && userStudentName.ToLower() != "q") // check for quit
                 {
-                    Console.WriteLine($"Please Enter {userStudentName} Grades separated by spaces");
-                    string userGrades = Console.ReadLine();
-                    students.Add(userStudentName, userGrades);//add student and grades
+                    while (true)
+                    {
+                        Console.WriteLine($"Please Enter {userStudentName} Grades separated by spaces");
+                        string userGrades = Console.ReadLine().Trim();
+                        try //check if grades are numbers
+                        {
+                            string[] testGrade = userGrades.Split(" ");
+                            foreach (string item in testGrade)
+                            {
+                                decimal test = Convert.ToDecimal(item);
+                            }
+                            students.Add(userStudentName, userGrades);//add student and grades
+                            break;
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("You entered a grade that was not a number");    
+                        }
+                    }
                 }
                 else
                 {
@@ -39,16 +54,16 @@ namespace Gradebook
                         Console.WriteLine("You did not enter anything - Goodbye!");
                     }
                 }
-            } while (add); //break whe false
+            } while (add); //break when false
         }
 
         static void Display()
         {      
             foreach(KeyValuePair<string, string> student in students)
             {
-                decimal minGrade = 100, maxGrade = 0, sum = 0;
+                decimal minGrade = 200, maxGrade = 0, sum = 0;
                 string studentName = student.Key; //set current student name
-                string[] studentGrade = student.Value.Split(" "); ; //set current student grades as an array
+                string[] studentGrade = student.Value.Split(" ");  //set current student grades as an array
                 decimal[] decGrades = studentGrade.Select(decimal.Parse).ToArray(); //convert string array to dec array
                 foreach(decimal grade in decGrades) // loop through grades
                 {
