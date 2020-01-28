d2695d773ae330bfc137d0bacb4881c7e1c30efc
gsanders32 <gray.sanders@gmail.com>
2020-01-27 20:33:16 -0600

    ToDoItem

--- C-Sharp-Programs/LCAUnit2/scratchpad/Program.cs
+++ C-Sharp-Programs/LCAUnit2/scratchpad/Program.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace scratchpad
 {
-    internal class Program
-    {
-        
-    }
+    
 }
