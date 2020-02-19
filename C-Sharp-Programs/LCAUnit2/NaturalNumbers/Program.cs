bfc161c5855a5313c8b7de59022982b01ecd6ed6
gsanders32 <gray.sanders@gmail.com>
2020-02-18 19:23:30 -0600

    DataBase Image

---
+++ C-Sharp-Programs/LCAUnit2/NaturalNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NaturalNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[2] { 3, 5 };
+            List<int> numsList = new List<int>();
+            int sum = 0;
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 1; j < 1000; j++)
+                {
+                    if (j * nums[i] < 1000)
+                    {
+                        numsList.Add(j * nums[i]);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            foreach (var item in numsList)
+            {
+                sum += item;
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
